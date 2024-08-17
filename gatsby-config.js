9ac2ebd704cc8508f54538cb4da997fadbbb9514
Eirini Tipidou <68666842+Raven-Ir@users.noreply.github.com>
2024-08-17 22:05:22 +0200

    Commit 001 - Set Up
    Project set up

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `mortgage-calculator`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss",
+    {
+      resolve: 'gatsby-plugin-manifest', 
+      options: {
+        "icon": "src/images/favicon-32x32.png"
+      }
+    }
+  ]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+       <h1></h1>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => (
+  <>
+    <title>Mortgage Payment</title>
+    <link id="icon" rel="icon" href="src/images/favicon-32x32.png"></link>
+    <meta name="description" content="Mortgage Payment"/>
+  </>
+)
+  
